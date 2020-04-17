8e62b7c41fc6138aa7964e992d631d3cb748a8a4
PULK4N <47610156+PULK4N@users.noreply.github.com>
2020-04-17 20:21:36 +0200

    unitStatusBar

--- YetAnotherAutoChess/MainWindow.xaml.cs
+++ YetAnotherAutoChess/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
-﻿using System;
+﻿using HelixToolkit.Wpf.SharpDX;
+using HelixToolkit.Wpf.SharpDX.Model.Scene;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
 
-namespace YetAnotherAutoChess
+namespace HelixTest
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +15,54 @@ namespace YetAnotherAutoChess
         public MainWindow()
         {
             InitializeComponent();
+
+            view.Camera = new HelixToolkit.Wpf.SharpDX.OrthographicCamera()
+            {
+                LookDirection = new System.Windows.Media.Media3D.Vector3D(0, -10, -10),
+                Position = new System.Windows.Media.Media3D.Point3D(0, 10, 10),
+                UpDirection = new System.Windows.Media.Media3D.Vector3D(0, 1, 0),
+                FarPlaneDistance = 5000,
+                NearPlaneDistance = 0.1f
+            };
+
+            DirectionalLight3D directionalLight3D = new DirectionalLight3D();
+            directionalLight3D.Direction = view.Camera.LookDirection;
+            directionalLight3D.Color = Color.FromRgb(214, 214, 214);
+            view.Items.Add(directionalLight3D);
+
+            view.EffectsManager = new DefaultEffectsManager();
+
+            MainViewModel mainViewModel1 =
+                new MainViewModel("C:/Users/Nikola/Documents/JA_AutoChess/JA_AutoChess/GProject/Assets/Mini Legion Lich PBR HP/Animations/Lich/Attack01.fbx");
+            MainViewModel mainViewModel2 =
+                new MainViewModel("C:/Users/Nikola/Documents/JA_AutoChess/JA_AutoChess/GProject/Assets/Mini Legion Footman HP PBR/Animations/Footman_Attack01.fbx");
+            mainViewModel2.MoveTo(new Vector3D(400, 0, 0));
+            StatusBarInfo statusBarInfo = new StatusBarInfo();
+            MainCanvas.Children.Add(statusBarInfo.canvas);
+            List<MainViewModel> models = new List<MainViewModel>();
+            models.Add(mainViewModel1);
+            models.Add(mainViewModel2);
+
+            foreach (MainViewModel model in models)
+            {
+                Element3DPresenter element3DPresenter = new Element3DPresenter();
+                element3DPresenter.Content = model.GroupModel;
+                view.Items.Add(element3DPresenter);
+            }
+            
+            view.AddHandler(Element3D.MouseDown3DEvent, new RoutedEventHandler((s, e) =>
+            {
+                var arg = e as MouseDown3DEventArgs;
+
+                if (arg.HitTestResult == null)
+                {
+                    return;
+                }
+                if (arg.HitTestResult.ModelHit is SceneNode node && node.Tag is AttachedNodeViewModel vm)
+                {
+                    vm.Selected = !vm.Selected;
+                }
+            }));
         }
     }
 }
---
+++ YetAnotherAutoChess/StatusBarInfo.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace HelixTest
+{
+    class StatusBarInfo
+    {
+        public Canvas canvas { get; }
+        private ProgressBar HealthBar;
+        private ProgressBar ManaBar;
+        public StatusBarInfo(double PositionLeft_To_Right = 100, double PositionBottom_To_Top = 400, double healthBar = 100, double manaBar = 100, int numOfStars = 0)
+        {
+            canvas = (Application.Current.Resources["unitStatusBar"] as Canvas);
+            setCanvasPosition(PositionLeft_To_Right, PositionBottom_To_Top);
+            SetChildren();
+            setValues(healthBar,manaBar);
+        }
+        public void setValues(double healthBar = 100, double manaBar = 100)
+        {
+            if (healthBar <= 100 && healthBar >= 0 && manaBar >= 0 && manaBar <= 100)
+            {
+                HealthBar.Value = healthBar;
+                ManaBar.Value = manaBar;
+            }
+        }
+
+        public void setCanvasPosition(double PositionLeft_To_Right = 100, double PositionBottom_To_Top = 100)
+        {
+            Canvas.SetLeft(canvas, PositionLeft_To_Right);
+            Canvas.SetBottom(canvas, PositionBottom_To_Top);
+        }
+
+        public void SetChildren()
+        {
+            foreach (UIElement child in canvas.Children)
+            {
+                Viewbox viewbox = child as Viewbox;
+                ProgressBar progress = viewbox.Child as ProgressBar;
+                if (progress.Name == "unitHealthBar")
+                {
+                    HealthBar = progress;
+                }
+                else if (progress.Name == "unitManaBar")
+                {
+                    ManaBar = progress;
+                }
+            }
+        }
+    }
+}
