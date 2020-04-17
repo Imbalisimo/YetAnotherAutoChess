a472ef845876640460217291608c3bea462d52c4
Marin Pepeljnjak <marin9619@hotmail.com>
2020-04-17 21:55:28 +0200

    Add all enums

--- YetAnotherAutoChess/Enums.cs
+++ YetAnotherAutoChess/Enums.cs
@@ -4,9 +4,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace YetAnotherAutoChess
+namespace YetAnotherAutoChess.Enums
 {
-    class Enums
+    public enum GameStates
     {
+        Menu,
+        Play,
+        Defeat,
+        Victory
+    }
+    public enum Levels
+    {
+        Menu,
+        BattleZone
+    }
+
+    public enum GameTypes
+    {
+        OneVsOne,
+        EightVsEight,
+        PVE,
+        None
+    }
+
+    public enum MatchState
+    {
+        Preparation,
+        Disposal,
+        Blessing,
+        Duplication,
+        Stretching,
+        Battle
+    }
+
+    public enum SynergyType
+    {
+        Mythology,
+        Deity
+    }
+
+    public enum Synergy
+    {
+        Aztec,
+        Celtic,
+        Chinese,
+        Egyptian,
+        Greek,
+        Hindu,
+        Japanese,
+        Maya,
+        Norse,
+        Sumerian,
+        Voodoo,
+
+        Beauty,
+        Chief,
+        Dragon,
+        Earth,
+        Harvest,
+        Hunt,
+        Thunder,
+        Trickster,
+        Underworld,
+        War,
+        Water,
+        Wisdom,
+
+        none
+    }
+
+    public enum SynergyBuffTarget
+    {
+        AllAllies,
+        SynergyHolders
+    }
+
+    public enum Piece
+    {
+        none,
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+        Queen
+    }
+
+    public enum Rarity
+    {
+        none,
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+    public enum ActiveUnits
+    {
+        Thor
+    }
+
+    public enum DamageType
+    {
+        none,
+        Physical,
+        Magical,
+        True
+    }
+
+    public enum TargetingSystem
+    {
+        Target,
+        ClosestEnemy,
+        HighestEnemyDps,
+        LowestAllyHp,
+        Self
     }
 }
