7ce4f8458c4278af0c24f6de166cbf96680320f2
Marin Pepeljnjak <marin9619@hotmail.com>
2020-04-17 21:35:40 +0200

    Add model to gameObject

--- YetAnotherAutoChess/Business/EngineCore/GameObject.cs
+++ YetAnotherAutoChess/Business/EngineCore/GameObject.cs
@@ -9,10 +9,15 @@ namespace YetAnotherAutoChess.Business
     public abstract class GameObject
     {
         public List<System.Windows.FrameworkElement> UIElements;
+        private MainViewModel _mainViewModel;
+        internal MainViewModel MainViewModel { get => _mainViewModel; set => _mainViewModel = value; }
+
         protected GameObject()
         {
+            UIElements = new List<System.Windows.FrameworkElement>();
             LoopManager.AddGameObject(this);
         }
+
 
         public abstract void Update();
     }
